44d4ef51900ef1ff6106f996a0f68ccc8ca078e5
Chrystilla Duchene <chrystilla.villez@hotmail.fr>
2023-02-13 18:39:21 +0100

    Responsive OK

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../about/about.css'
 
 import Banner from '../../components/banner/Banner';
-import banner from '../../assets/banner_about.png'
+import banner from '../../assets/about.png'
 import aboutData from '../../data/aboutData'
 import Collapse from '../../components/collapse/Collapse';
 
